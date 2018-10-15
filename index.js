4165f014d840312ef1817b3693eb696f2d5e8ce3
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-15 00:08:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay (musicians, instruments) {
   var array = []
-  var l = array.length
+  var l = musicians.length
   for (let i = 0; i < l; i ++)
   array.push
   
