fb0e5f0746f934f95111f92502b2b655cb896254
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-15 00:18:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function theBeatlesPlay (musicians, instruments) {
   var array = []
   var l = musicians.length
-  for (let i = 0; i < l; i ++)
-  array.push(`${musicians[i]})
+  for (let i = 0; i < l; i++)
+  array.push(`${musicians[i]}`)
   
 return array
 }
